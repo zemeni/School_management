008f826f2a245e2de7f707a35ed39a01798544ca
Babu <neupanebabu828@gmail.com>
2021-03-10 23:41:35 +1100

    initializing project

---
+++ src/main/java/com/babu/schoolmanagement/School_management/SchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.babu.schoolmanagement.School_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApplication.class, args);
+	}
+
+}
