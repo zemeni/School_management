0aa9c7522efb44a2c1e86903d8b66dce0115c314
Babu <neupanebabu828@gmail.com>
2021-03-13 05:55:13 +1100

    adding service test layer

--- src/test/java/com/babu/schoolmanagement/School_management/repository/StudentRepositoryTest.java
+++ src/test/java/com/babu/schoolmanagement/School_management/repository/StudentRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -22,10 +24,37 @@ class StudentRepositoryTest {
     private StudentRepository studentRepository;
 
     @Test
-    void findByEmail() {
-        Student student = new Student(1,"Babu","Neupane","abc@gmail.com");
-        Student savedInDb = testEntityManager.merge(student);
+    void saveStudent() {
+        Student student = new Student("Babu","Neupane","abc@gmail.com");
+        Student savedInDb = testEntityManager.persist(student);
         Student fromDb = studentRepository.getOne(savedInDb.getId());
         assertThat(fromDb).isEqualTo(savedInDb);
     }
+
+    @Test
+    void findByEmail(){
+        Student student = new Student("Babu","Neupane","abc@gmail.com");
+        Student savedInDb = testEntityManager.persist(student);
+        Student fromDb = studentRepository.findByEmail("abc@gmail.com");
+        assertThat(savedInDb).isEqualTo(fromDb);
+    }
+
+    @Test
+    void findById(){
+        Student student1 = new Student("abc","bcd","doid");
+        Student savedInDb = testEntityManager.persist(student1);
+        Student fromDb = studentRepository.getOne(savedInDb.getId());
+        assertThat(fromDb).isEqualTo(savedInDb);
+    }
+
+    @Test
+    void findAll(){
+        Student student2 = new Student("Baburam","Neupane","bcd@gmail.com");
+        Student student3 = new Student("Baburam","Neupane","bcd@gmail.com");
+        Student savedInDb1 = testEntityManager.persist(student3);
+        Student savedInDb2 = testEntityManager.persist(student2);
+        List<Student> allFromDb = studentRepository.findAll();
+        assertThat(allFromDb.size()).isEqualTo(2);
+    }
+
 }
---
+++ src/test/java/com/babu/schoolmanagement/School_management/service/StudentServiceTest.java
@@ -0,0 +1,53 @@
+package com.babu.schoolmanagement.School_management.service;
+
+import com.babu.schoolmanagement.School_management.entity.Student;
+import com.babu.schoolmanagement.School_management.repository.StudentRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class StudentServiceTest {
+
+    @Autowired
+    private StudentService studentService;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @Test
+    void saveStudent() {
+        Student student = new Student("Babu","Neupane","abc@gmail.com");
+        when(studentRepository.save(student)).thenReturn(student);
+        assertThat(studentService.saveStudent(student)).isEqualTo(student);
+    }
+
+    @Test
+    void getStudentById() {
+        Student student = new Student(1,"Babu","Neupane","abc@gmail.com");
+        when(studentRepository.findById(1)).thenReturn(java.util.Optional.of(student));
+        assertThat(studentService.getStudentById(1)).isEqualTo(student);
+    }
+
+    @Test
+    void getAllStudents() {
+        Student student = new Student("Babu","Neupane","abc@gmail.com");
+        Student student1 = new Student("Babu","Neupane","abc@gmail.com");
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        students.add(student1);
+        when(studentRepository.findAll()).thenReturn(students);
+        assertThat(studentService.getStudents()).isEqualTo(students);
+    }
+}
